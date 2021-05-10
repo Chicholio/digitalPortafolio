fbdf41d45cfbe541e369a82edfcfd54b68e585e9
Mauricio Londoño Cianci <chicholoncia@gmail.com>
2021-05-09 20:18:10 -0500

    Cambiando de lugar el index.html otra vez

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './index.html',
+      template: './public/index.html',
       filename: './index.html',
     }),
     new MiniCssExtractPlugin({
